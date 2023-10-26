e30fdea994993a7d845a6992ce4517138463ebfa
anhtuan141 <anhtuan1411994@gmail.com>
2023-10-26 21:35:31 +0700

    Create class DiceRoller and class _DiceRollerState

---
+++ lib/dice_roller.dart
@@ -0,0 +1,17 @@
+import 'dart:math';
+
+import 'package:flutter/material.dart';
+
+class DiceRoller extends StatefulWidget {
+  const DiceRoller({super.key});
+
+  @override
+}
+
+class _DiceRollerState extends State<DiceRoller> {
+
+  @override
+  Widget build(context){
+
+  }
+}
